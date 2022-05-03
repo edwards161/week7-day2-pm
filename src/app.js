d2a3e14ea265057a26c752562b61632142f2e869
edwards161 <acejakey@gmail.com>
2022-05-03 15:57:22 +0100

    updates from last week

--- src/app.js
+++ src/app.js
@@ -2,19 +2,62 @@ const yargs = require("yargs");
 
 const { connectionTest, client} = require("./db/connection");
 //don't need to put index.js as it save characters and looks for it regardless!
-const { addMovie, listMovie, listMovies } = require ("./utils");
+const { addMovie, listMovie, listMovies, updateMovie, deleteMovie } = require ("./utils");
 
 const app = async (yargsObj) => {
-    const collection = await connectionTest();
-    if (yargsObj.add) {
-        await addMovie(collection, {title: yargsObj.title, actor: yargsObj.actor });
-        console.log("successfully added new document to database(db)")
-    } else if (yargsObj.list){
-        await listMovies(collection)
-    } else {
-        console.log("Incorrect Command")
+    try {
+      //creating the collection and saving to constant
+      const collection = await connection();
+      if (yargsObj.add) {
+        //add movie to database with yargs input
+        await addMovie(collection, {
+          title: yargsObj.title,
+          actor: yargsObj.actor,
+        });
+        console.log(`Successfully added ${yargsObj.title}`);
+      } else if (yargsObj.list) {
+        //console log the return statement from my list function
+        console.log(await listMovies(collection, yargsObj.searchTitle));
+      } else if (yargsObj.update) {
+        //update movie by using title to search and actor to update
+        await updateMovie(
+          collection,
+          { title: yargsObj.title },
+          { actor: yargsObj.actor }
+        );
+      } else if (yargsObj.delete) {
+        //delete movie by finding it with a title from yargs input
+        await deleteMovie(collection, { title: yargsObj.title });
+      } else {
+        console.log("Incorrect command");
+      }
+      await client.close();
+    } catch (error) {
+      console.log(error);
+      await client.close();
     }
-    await client.close();
-};
+  };
+  
+  app(yargs.argv);
+  
+// const app = async (yargsObj) => {
+//     const collection = await connectionTest();
+//     if (yargsObj.add) {
+//         await addMovie(collection, {title: yargsObj.title, actor: yargsObj.actor });
+//         console.log("successfully added new document to database(db)")
+//     } else if (yargsObj.list){
+//         await listMovies(collection)
+//     } else if (yargsObj.update){
+//         await updateMovie(collection, {title: yargsObj.title, actor: yargsObj.actor });
+//         console.log("sucessfully updated document to db")
+//     } else if (yargsObj.find){
+//         await listMovies(collection)
+//     } else if (yargsObj.delete){
+//         await deleteMovie(collection, {title: yargsObj.title, actor: yargsObj.actor });
+//     } else {
+//         console.log("Incorrect Command")
+//     }
+//     await client.close();
+// };
 
-app(yargs.argv);
+// app(yargs.argv);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,84 @@
-exports.addMovie = async (collection, moviedb) => {
+exports.addMovie = async (collection, movieObj) => {
     try {
-        const addEntry = await collection.insertOne(moviedb);
-        console.log(addEntry);
+      await collection.insertOne(movieObj);
     } catch (error) {
-        console.log(error)
+      throw new Error(error);
     }
-};
+  };
+  
+  exports.listMovies = async (collection, search) => {
+    try {
+      if (search) {
+        return await collection.find({ title: search }).toArray();
+      } else {
+        return await collection.find({}).toArray();
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  
+  exports.updateMovie = async (collection, filterObj, updateObj) => {
+    try {
+      const updateCheck = await collection.updateOne(filterObj, {
+        $set: updateObj,
+      });
+      if (updateCheck.modifiedCount > 0) {
+        console.log("Successfully updated");
+      } else {
+        console.log("No movie found");
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  
+  exports.deleteMovie = async (collection, filterObj) => {
+    try {
+      const deleteCheck = await collection.deleteOne(filterObj);
+      if (deleteCheck.deletedCount > 0) {
+        console.log("Successfully deleted");
+      } else {
+        console.log("No movie found");
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  
+// exports.addMovie = async (collection, movieObj) => {
+//     try {
+//         const addEntry = await collection.insertOne(movieObj);
+//         console.log(addEntry);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// };
 
-exports.listMovies = async (collection) => {
-    try {
-        const movieList = await collection.find().toArray();
-        console.log(movieList);
-    } catch (error) {
-        console.log(error)
-    }
-};
+// exports.listMovies = async (collection, filterObj, updateObj) => {
+//     try {
+//         const movieList = await collection.find({ title: search }).toArray();
+//         console.log(movieList);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// };
+
+// exports.updateMovie = async (collection, moviedb) => {
+//     try {
+//         const updateEntry = await collection.updateOne(moviedb, filter, update);
+//         console.log(updateEntry)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// exports.deleteMovie = async (collection, movieObj) => {
+//     try {
+//         const deleteEntry = await collection.deleteOne(movieObj)
+//         console.log(deleteEntry)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
